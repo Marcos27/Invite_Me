72e5e8e45d71b1acf1e985d191b0670f9ae1242a
Marcos Felix <marcosfelix299@yahoo.com>
2016-06-27 02:46:22 -0700

    testing

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,12 @@
  console.log("Hello World")
 
-
+$().ready(function(event) {
+  $(".heart").hide()
+   $(document).ready(function(){
+      $(".slider").click(function() {
+        $(".heart-holder").hide()
+        $(".heart").show()
+        // $(".heart-holder").animate({"width": "250px", "height": "250px"});
+      });
+  });
+});
