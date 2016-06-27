8f5780f76661d7792cbe8f52dae83dda02392f7a
Marcos Felix <marcosfelix299@yahoo.com>
2016-06-26 19:59:49 -0700

    added toast

--- js/javascript.js
+++ js/javascript.js
@@ -9,3 +9,6 @@
   // Hide sideNav
   $('.button-collapse').sideNav('hide');
 
+ // Materialize.toast(message, displayLength, className, completeCallback);
+  Materialize.toast('I am a toast!', 4000) // 4000 is the duration of the toast
+
